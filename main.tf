b9d7b6a8d11e0941648516f971daa280a4e8ad2f
zaidzahid <thebatmanoutraech@gmail.com>
2025-08-30 13:58:53 +0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,119 @@
+erraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-north-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-north-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+
+  tags = {
+    Name = "Public-Route-table"
+  }
+}
+
+resource "aws_route_table_association" "table-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_instance" "mywebserver" {
+  ami                         = "ami-0a716d3f3b16d290c"  # Example Ubuntu AMI for eu-north-1
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash 
+              apt update -y
+              apt install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "mywebserver"
+  }
+}
